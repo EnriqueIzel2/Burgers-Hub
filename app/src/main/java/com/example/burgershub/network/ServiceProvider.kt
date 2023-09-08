c763d18fa4331cf9c7621158d33bfefc7f6102c9
Enrique Izel <eleaoizel@gmail.com>
2023-09-08 14:49:17 -0400

    feat: create interceptor to show logs and add header

--- app/src/main/java/com/example/burgershub/network/ServiceProvider.kt
+++ app/src/main/java/com/example/burgershub/network/ServiceProvider.kt
@@ -1,6 +1,8 @@
 package com.example.burgershub.network
 
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
@@ -11,6 +13,25 @@ class ServiceProvider {
   private val client = OkHttpClient.Builder()
     .connectTimeout(30, TimeUnit.SECONDS)
     .readTimeout(30, TimeUnit.SECONDS)
+    .addInterceptor(HttpLoggingInterceptor().apply {
+      level = HttpLoggingInterceptor.Level.BODY
+    })
+    .addInterceptor(Interceptor { chain ->
+      chain.run {
+        proceed(
+          request()
+            .newBuilder()
+            .addHeader(
+              name = "X-RapidAPI-Key",
+              value = "b3f5a1bf3cmsh5dd78db296640b5p1937e1jsn68263cee9135"
+            ).addHeader(
+              name = "X-RapidAPI-Host",
+              value = "burgers-hub.p.rapidapi.com"
+            )
+            .build()
+        )
+      }
+    })
     .build()
 
   private val retrofit = Retrofit.Builder()
