1780f4f34c309460d961fe2f108b334407016987
Enrique Izel <eleaoizel@gmail.com>
2023-09-08 14:47:56 -0400

    feat: init recycler on BurgersFragment

--- app/src/main/java/com/example/burgershub/presenter/burgers/BurgersFragment.kt
+++ app/src/main/java/com/example/burgershub/presenter/burgers/BurgersFragment.kt
@@ -1,32 +1,71 @@
 package com.example.burgershub.presenter.burgers
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.burgershub.R
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.burgershub.databinding.FragmentBurgersBinding
+import com.example.burgershub.domain.model.Burger
+import com.example.burgershub.util.StateView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class BurgersFragment : Fragment() {
 
-    private var _binding: FragmentBurgersBinding? = null
-    private val binding get() = _binding!!
+  private var _binding: FragmentBurgersBinding? = null
+  private val binding get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentBurgersBinding.inflate(inflater, container, false)
-        return binding.root
-    }
+  private val viewModel: BurgerViewModel by viewModels()
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View {
+    _binding = FragmentBurgersBinding.inflate(inflater, container, false)
+    return binding.root
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+
+    getBurgers()
+  }
+
+  private fun getBurgers() {
+    viewModel.getBurgers().observe(viewLifecycleOwner) { stateView ->
+      when (stateView) {
+        is StateView.Loading -> {}
+        is StateView.Success -> {
+          binding.fragmentBurgersProgressBar.isVisible = false
+
+          val burgers = stateView.data ?: emptyList()
+          initRecycler(burgers)
+        }
+        is StateView.Error -> {
+          binding.fragmentBurgersProgressBar.isVisible = false
+          Toast.makeText(requireContext(), stateView.message, Toast.LENGTH_SHORT).show()
+        }
+      }
     }
+  }
+
+  private fun initRecycler(burgers: List<Burger>) {
+    with(binding.fragmentBurgersRVBurgers) {
+      setHasFixedSize(true)
+      adapter = BurgersAdapter(burgers) { burgerId ->
+
+      }
+    }
+  }
+
+  override fun onDestroyView() {
+    super.onDestroyView()
+    _binding = null
+  }
 
 }
