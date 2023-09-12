30bead2171ffeb4637b3e136f2823f16f753878f
Enrique Izel <eleaoizel@gmail.com>
2023-09-12 16:57:42 -0400

    feat: add mock for API

--- app/src/main/java/com/example/burgershub/data/api/ServiceAPI.kt
+++ app/src/main/java/com/example/burgershub/data/api/ServiceAPI.kt
@@ -12,9 +12,11 @@ interface ServiceAPI {
   @GET("burgers")
   suspend fun getBurgers(): List<BurgerResponse>
 
+  @MOCK(asset = "burger.json", runDelay = true)
   @GET("burgers/{burger_id}")
   suspend fun getBurgerById(@Path("burger_id") burgerId: Int): BurgerResponse
 
+  @MOCK(asset = "burger_search.json", runDelay = true)
   @GET("find-burger/")
   suspend fun getBurgerByName(@Query("search") name: String): List<BurgerResponse>
 }
