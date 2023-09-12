f3a411dfa244e20abb90da7bd7e60b3f7853341d
Enrique Izel <eleaoizel@gmail.com>
2023-09-12 16:59:20 -0400

    feat: create initListeners

--- app/src/main/java/com/example/burgershub/presenter/burgers/BurgersFragment.kt
+++ app/src/main/java/com/example/burgershub/presenter/burgers/BurgersFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -35,20 +36,75 @@ class BurgersFragment : Fragment() {
     super.onViewCreated(view, savedInstanceState)
 
     getBurgers()
+    initListeners()
+  }
+
+  private fun initListeners() {
+    binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+      override fun onQueryTextSubmit(name: String?): Boolean {
+        return if (name != null) {
+          getBurgerByName(name)
+
+          return true
+        } else false
+      }
+
+      override fun onQueryTextChange(newText: String?): Boolean {
+        return false
+      }
+    })
+
+    val closeButton: View? = binding.searchView.findViewById(androidx.appcompat.R.id.search_close_btn)
+    closeButton?.setOnClickListener {
+      binding.searchView.setQuery("", false)
+      binding.searchView.clearFocus()
+      getBurgers()
+    }
   }
 
   private fun getBurgers() {
     viewModel.getBurgers().observe(viewLifecycleOwner) { stateView ->
       when (stateView) {
-        is StateView.Loading -> {}
+        is StateView.Loading -> {
+          binding.fragmentBurgersRVBurgers.isVisible = false
+          binding.fragmentBurgersProgressBar.isVisible = true
+        }
         is StateView.Success -> {
           binding.fragmentBurgersProgressBar.isVisible = false
+          binding.fragmentBurgersRVBurgers.isVisible = true
 
           val burgers = stateView.data ?: emptyList()
           initRecycler(burgers)
         }
+
         is StateView.Error -> {
           binding.fragmentBurgersProgressBar.isVisible = false
+          binding.fragmentBurgersRVBurgers.isVisible = true
+          Toast.makeText(requireContext(), stateView.message, Toast.LENGTH_SHORT).show()
+        }
+      }
+    }
+  }
+
+  private fun getBurgerByName(name: String) {
+    viewModel.getBurgersByName(name).observe(viewLifecycleOwner) { stateView ->
+      when (stateView) {
+        is StateView.Loading -> {
+          binding.fragmentBurgersRVBurgers.isVisible = false
+          binding.fragmentBurgersProgressBar.isVisible = true
+        }
+
+        is StateView.Success -> {
+          binding.fragmentBurgersProgressBar.isVisible = false
+          binding.fragmentBurgersRVBurgers.isVisible = true
+
+          val burgers = stateView.data ?: emptyList()
+          initRecycler(burgers)
+        }
+
+        is StateView.Error -> {
+          binding.fragmentBurgersProgressBar.isVisible = false
+          binding.fragmentBurgersRVBurgers.isVisible = true
           Toast.makeText(requireContext(), stateView.message, Toast.LENGTH_SHORT).show()
         }
       }
